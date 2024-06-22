442e61f85b07ef418da2853e268f400cd8721c65
bouychandara <darasmart2020@gmail.com>
2024-06-22 12:33:13 +0700

    organize files structure

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,9 +20,9 @@ export default function Footer()
     }
 
     return(
-        <footer className="w-full flex px-6 bg-[#1d2128] py-3">
-            <div className="w-1/2 flex justify-center">
-                <div className="flex flex-col justify-between">
+        <footer className="w-full flex px-6 bg-[#1d2128] py-3 border-0">
+            <div className="w-1/2 flex justify-center items-center">
+                <div className="flex flex-col justify-between h-5/6">
                     <p className="text-3xl font-semibold max-w-[16.75rem] text-white">Say hello and let's work together!</p>
                     <div className="flex flex-col gap-y-3">
                         <div className="flex gap-x-3">
