b733dc166012e1d42c7bb351315f1a69808a999b
bouychandara <darasmart2020@gmail.com>
2024-06-22 18:46:04 +0700

    update navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,6 +12,7 @@ export default function navbar()
     {
         pathname = window.location.pathname;
         pathname = pathname.replace(/^\//, '');
+        document.title = pathname.toUpperCase();
     }
 
     return(
